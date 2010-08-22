6f4a7e139d8d37acb36c986d8f5a3bb1d3a20f4e
huangyingw@vsts_2008_chs <huangyingw@gmail.com>
2010-08-22 11:12:24 -0400

    n

--- queen/queen.cpp
+++ queen/queen.cpp
@@ -2,9 +2,9 @@
 //
 
 #include "stdafx.h"
-static int a[8];
-static int b[15];
-static int c[15];
+static int column_strike[8];
+static int master_diagonal[15];
+static int slave_diagonal[15];
 static int iQueenNum=0;  //记录总的棋盘状态数
 static char Queen[8][8];
 
@@ -17,14 +17,14 @@ int _tmain(int argc, _TCHAR* argv[])
 	//棋盘初始化，空格为*，放置皇后的地方为@
 	for(nav=0;nav<8;nav++)
 	{
-	  a[nav]=0;  //列标记初始化，表示无列冲突
+	  column_strike[nav]=0;  //列标记初始化，表示无列冲突
 	  for(iColumn=0;iColumn<8;iColumn++)
 			Queen[nav][iColumn]='*';
 	}
 	
 	//主、从对角线标记初始化，表示没有冲突
 	for(nav=0;nav<15;nav++)
-		b[nav]=c[nav]=0;
+		master_diagonal[nav]=slave_diagonal[nav]=0;
 
 	qu(0);
 	return 0;
@@ -34,12 +34,12 @@ void qu(int i)
 {
 	for(int iColumn=0;iColumn<8;iColumn++)
 	{
-	  if(a[iColumn]==0&&b[i-iColumn+7]==0&&c[i+iColumn]==0) //如果无冲突
+	  if(column_strike[iColumn]==0&&master_diagonal[i-iColumn+7]==0&&slave_diagonal[i+iColumn]==0) //如果无冲突
 	  {
 			Queen[i][iColumn]='@'; //放皇后
-			a[iColumn]=1;   //标记，下一次该列上不能放皇后
-			b[i-iColumn+7]=1;  //标记，下一次该主对角线上不能放皇后
-			c[i+iColumn]=1;   //标记，下一次该从对角线上不能放皇后
+			column_strike[iColumn]=1;   //标记，下一次该列上不能放皇后
+			master_diagonal[i-iColumn+7]=1;  //标记，下一次该主对角线上不能放皇后
+			slave_diagonal[i+iColumn]=1;   //标记，下一次该从对角线上不能放皇后
 			if(i<7) qu(i+1);  //如果行还没有遍历完，进入下一行
 			else     //否则输出
 			{
@@ -56,9 +56,9 @@ void qu(int i)
 	  	}
 	  	//如果前次的皇后放置导致后面的放置无论如何都不能满足要求，则回溯，重置
 	  	Queen[i][iColumn]='*';
-	  	a[iColumn]=0;
-	  	b[i-iColumn+7]=0;
-	  	c[i+iColumn]=0;
+	  	column_strike[iColumn]=0;
+	  	master_diagonal[i-iColumn+7]=0;
+	  	slave_diagonal[i+iColumn]=0;
 		}
 	}
 } 
